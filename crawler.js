1591b11bf9b4cf5a53b8a8eedf84e8f8ea619f97
IdanLnrd <idangibly.dev@gmail.com>
2022-11-06 17:44:05 +0200

    adding file writer

--- crawler.js
+++ crawler.js
@@ -1,4 +1,4 @@
-const { writeFileSync } = require('fs');
+const { writeFileSync, readFileSync } = require('fs');
 const htmlparser2 = require("htmlparser2");
 const axios = require('axios');
 
@@ -44,16 +44,32 @@ class WebPage {
   }
 }
 
+class CrawlerHelper {
+  static updateResultsFile({ outputFilePath, results }) {
+    const buff = readFileSync(outputFilePath);
+    const { results: res } = JSON.parse(buff.toString()) || {};
+    res.push(...results);
+    writeFileSync(outputFilePath, JSON.stringify({ results: res }));
+  }
+  static clearResultsFile({ outputFilePath }) {
+    writeFileSync(outputFilePath,JSON.stringify({ results: [] }));
+  }
+}
+
 class Crawler {
   async extractImages({ outputFilePath, depth = 0, url }) {
+    CrawlerHelper.clearResultsFile({ outputFilePath });
+    if (depth < 0) {
+      return;
+    }
+    const _depth = Math.floor(depth);
     const page = await new WebPage().getPage(url);
-    const { images } = await new HtmlParser().extract(page, [
-      { tag: 'img', attr: 'src', name: 'images' }
+    const { images, links } = await new HtmlParser().extract(page, [
+      { tag: 'img', attr: 'src', name: 'images' },
+      { tag: 'a', attr: 'href', name: 'links' }
     ]);
-    const results = images.map(src => ({ imageUrl: src, sourceUrl: url, depth }));
-    writeFileSync(outputFilePath, JSON.stringify({
-      results
-    }));
+    const results = images.map(src => ({ imageUrl: src, sourceUrl: url, depth: _depth }));
+    CrawlerHelper.updateResultsFile({ outputFilePath, results });
   }
 }
 
